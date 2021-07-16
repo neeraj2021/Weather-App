97f6460c432f3ce34587016c7136cd2de7968a8c
Neeraj Kumar Mahawar <neerajkmahawar2017@gmail.com>
2021-07-16 23:24:25 +0530

    background_add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import Weather from "./components/Weather";
+import "./App.css";
 
 function App() {
-  return <Weather />;
+  return (
+    <div className="background">
+      <Weather />;
+    </div>
+  );
 }
 
 export default App;
